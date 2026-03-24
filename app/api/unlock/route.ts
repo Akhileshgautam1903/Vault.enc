abf3766618c56cf604d0be9c6fd5a5a9fbda459e
Akhilesh Gautam <akhileshgautam19@gmail.com>
2026-03-24 07:31:55 +0530

    Add api routes

---
+++ app/api/unlock/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import { decryptVault } from "@/lib/crypto";
+
+import fs from "fs";
+import path from "path";
+
+export async function POST(request: Request) {
+  // 1. Get masterPassword from request body
+  const { masterPassword } = await request.json();
+
+  const filePath = path.join(process.cwd(), "data", "vault.enc");
+
+  // 2. Check vault.enc exists — use fs.existsSync directly
+  const fileExists = fs.existsSync(filePath);
+
+  // 3. Read the file — fs.readFileSync(filePath, "utf-8")
+  if (!fileExists) {
+    return NextResponse.json(
+      { error: "File does not exists" },
+      { status: 404 },
+    );
+  }
+  const cipherTxt = fs.readFileSync(filePath, "utf-8");
+
+  // 4. Call decryptVault() from your crypto.ts and return decrypted data or 401 if wrong password
+  var decryptedData;
+  try {
+    const decryptedData = decryptVault(cipherTxt, masterPassword);
+    return NextResponse.json({ data: decryptedData });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Incorrect master password" },
+      { status: 401 },
+    );
+  }
+}
