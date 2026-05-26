44f0b2640385560013c50a5c4b231ebf04591ea0
Akhilesh Gautam <akhileshgautam19@gmail.com>
2026-05-27 01:00:26 +0530

    Create Vault Logo Component

---
+++ components/vault/VaultLogo.tsx
@@ -0,0 +1,11 @@
+const VaultLogo = () => {
+  return (
+    <div className="absolute left-6 top-6 z-10">
+      <h1 className="text-4xl font-serif">
+        Vault<span className="accent-text">.enc</span>
+      </h1>
+    </div>
+  );
+};
+
+export default VaultLogo;
