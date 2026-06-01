0406a26b6d92b16cc84f8359fdc8d02be91ae8fa
Akhilesh Gautam <akhileshgautam19@gmail.com>
2026-06-01 23:12:14 +0530

    Create Hero component

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -1,26 +1,30 @@
-import React from "react";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section className="my-12 text-center">
-      <h1 className="text-5xl font-serif mb-6">
-        Your passowrds never leave your{" "}
-        <span className="accent-text">machine.</span>
-      </h1>
-      <p className="mb-6">
-        Every other password manager stores your data on their servers. <br />
-        We don't even have servers. <br />
-        Just you and an encrypted file on your machine.
-      </p>
-      <div className="w-full flex justify-center gap-4 font-serif">
-        <Button className="text-lg" asChild>
-          <Link href="/setup">Start Fresh</Link>
-        </Button>
-        <Button className="text-lg" variant="secondary" asChild>
-          <Link href="/unlock">Unlock your vault</Link>
-        </Button>
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+      <div>
+        <h1 className="text-4xl lg:text-7xl font-serif mb-6">
+          Your passwords never leave your{" "}
+          <span className="accent-text">system.</span>
+        </h1>
+        <p className="mb-4 text-base">
+          Every other password manager stores your data on their servers. 
+          We don't even have servers. 
+          Just you and an encrypted file on your machine.
+        </p>
+        <div className="w-full flex gap-4 font-serif">
+          <Button className="text-lg" asChild>
+            <Link href="/setup">Start Fresh</Link>
+          </Button>
+          <Button className="text-lg" variant="outline" asChild>
+            <Link href="/unlock">Unlock your vault</Link>
+          </Button>
+        </div>
+      </div>
+      <div className="w-full rounded-xl border border-zinc-800 bg-zinc-900 aspect-video flex items-center justify-center text-zinc-600">
+        Preview coming soon
       </div>
     </section>
   );
