d2e0305e29112e5004b6cb86d2ba39bd9af754c9
Akhilesh Gautam <akhileshgautam19@gmail.com>
2026-06-02 08:42:08 +0530

    Make navbar responsive for mobile

--- components/landing/Navbar.tsx
+++ components/landing/Navbar.tsx
@@ -1,27 +1,74 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 type NavbarProps = {
   showLinks?: boolean;
 };
 
 const Navbar = ({ showLinks = false }: NavbarProps) => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <nav className="flex justify-between items-center pt-6 font-serif">
-      <h1 className="text-4xl font-serif">
-        Vault<span className="accent-text">.enc</span>
-      </h1>
-      {showLinks && (
-        <div className="flex items-center gap-4 text-lg">
+    <div className="font-serif">
+      {/* Top row - always visible */}
+      <nav className="flex justify-between items-center pt-6">
+        <h1 className="text-4xl font-serif">
+          Vault<span className="accent-text">.enc</span>
+        </h1>
+        {showLinks && (
+          <>
+            {/* Mobile hamburger */}
+            <div className="lg:hidden">
+              <Menu
+                className="cursor-pointer"
+                onClick={() => setToggleMenu((prev) => !prev)}
+              />
+            </div>
+
+            {/* Desktop links */}
+            <div className="hidden lg:flex items-center gap-4 text-lg">
+              <a
+                href=""
+                className="hover:text-accent transition-colors duration-150 ease-in-out"
+              >
+                Security
+              </a>
+              <a
+                href=""
+                className="hover:text-accent transition-colors duration-150 ease-in-out"
+              >
+                How it works
+              </a>
+              <a
+                href=""
+                className="hover:text-accent transition-colors duration-150 ease-in-out"
+              >
+                FAQs
+              </a>
+              <Button className="text-lg" asChild>
+                <Link href="/setup">Start Fresh</Link>
+              </Button>
+            </div>
+          </>
+        )}
+      </nav>
+
+      {/* Mobile dropdown - sits BELOW the nav row */}
+      {showLinks && toggleMenu && (
+        <div className="flex flex-col lg:hidden gap-4 pt-4 text-lg border-b border-b-accent pb-4">
           <a href="">Security</a>
           <a href="">How it works</a>
           <a href="">FAQs</a>
-          <Button className="text-lg" asChild>
+          <Button asChild>
             <Link href="/setup">Start Fresh</Link>
           </Button>
         </div>
       )}
-    </nav>
+    </div>
   );
 };
 
